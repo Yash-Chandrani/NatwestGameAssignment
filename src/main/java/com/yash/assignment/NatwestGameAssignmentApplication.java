494d0847beb6e57c1358a2a69a3ca5c459c7e979
Yash Chandrani <yashbeskkc@gmail.com>
2022-12-20 01:09:41 +0530

    Added Logger

--- src/main/java/com/yash/assignment/NatwestGameAssignmentApplication.java
+++ src/main/java/com/yash/assignment/NatwestGameAssignmentApplication.java
@@ -1,13 +1,17 @@
 package com.yash.assignment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NatwestGameAssignmentApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(NatwestGameAssignmentApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(NatwestGameAssignmentApplication.class, args);
+		logger.info("Rock Paper Scissors Game started");
 	}
 
 }
