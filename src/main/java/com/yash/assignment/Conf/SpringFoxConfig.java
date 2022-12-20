e2cb4956a8ebcf012d1df727594f0418422e36c7
Yash Chandrani <yashbeskkc@gmail.com>
2022-12-20 17:44:58 +0530

    Added API metadata, comments and formatting

--- src/main/java/com/yash/assignment/Conf/SpringFoxConfig.java
+++ src/main/java/com/yash/assignment/Conf/SpringFoxConfig.java
@@ -1,13 +1,20 @@
 package com.yash.assignment.Conf;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//Swagger API configuration for documentation
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
@@ -17,6 +24,20 @@ public class SpringFoxConfig {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiDetails());
 	}
+	//API metadata
+	private ApiInfo apiDetails() {
+	    return new ApiInfo(
+	      "NatwestGameAssignment Microservice Api", 
+	      "Rock Paper Scissors game microservice as an assignment for internship at Natwest", 
+	      "v 1.0.0", 
+	      "Free to use", 
+	      new Contact("Yash Chandrani", "https://github.com/Yash-Chandrani", "yashbeskkc@gmail.com"), 
+	      "License of API", "None", Collections.emptyList());
+	}
+	
 }
+
+
